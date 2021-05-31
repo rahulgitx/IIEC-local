63b63ce3754856d9eaf3501b9fffd59d02c8f210
rahul <rahul18bhardwaj.23@gmail.com>
2021-05-31 11:22:49 +0530

    commmt

--- flask/flask_mongodb/app.py
+++ flask/flask_mongodb/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 import flask
 from flask_pymongo import PyMongo
+import json
 
 app=Flask("mongoapp")
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mymongodb"
@@ -11,10 +12,12 @@ db=mongo.db
 
 '''
 #CREATE
-db.mycoll.insert_one({'name':'rahul', 'roll_no':182056})   #creates a table mamed mycoll and insert one entry
+y={'name':'rahul', 'roll_no':182056}
+x=db.mycoll.insert_one({'name':'rahul', 'roll_no':182056})   #creates a table mamed mycoll and insert one entry
+print('data inserted with id :', x)
 db.mycoll.insert_many([{'name':'jack', 'roll no':1},
                       {'name':'robert', 'roll no':2}])     # creates multiples entries in the collection mycoll. 
-'''
+#'''
 
 
 '''
@@ -45,4 +48,20 @@ db.mycoll.delete_one(myquery)                               # deletes an entry w
 '''
 x = db.mycoll.delete_many({})
 print(x.deleted_count, " documents deleted.")               # deletes the whole collection and prints out deleted documents.
-'''
+#'''
+
+#'''
+@app.route('/')
+def form():
+    data=render_template('form.html')
+    return data
+
+@app.route('/result', methods=['GET'])
+def result():
+    query=json.loads(request.args.get('query'))
+    x=db.mycoll.insert_one(query)
+    print(jsonify(x))
+    return "successfully inserted document : " jsonify(x)
+
+app.run(debug=True, port=500)
+#'''
